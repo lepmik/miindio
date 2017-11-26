8b2191dbc023b601c929666e9e1ce8d792c75a06
Mikkel E Lepperød <lep.mik@gmail.com>
2017-11-25 20:45:43 -0800

    add run command

--- miindio/miindcli.py
+++ miindio/miindcli.py
@@ -11,9 +11,24 @@ def cli():
              '"xml" parameter file.')
 @click.argument("xml_path", type=click.Path(exists=True))
 @click.option("--directory", "-d", type=click.Path(exists=True))
+@click.option("--run", "-r", isflag=True)
 def generate_(name, xml_path, **kwargs):
     io = MiindIO(xml_path, kwargs['directory'])
     io.generate()
+    if kwargs['run']:
+        io.run()
+
+
+@cli.command("run", short_help='Run a MIIND executable based on a ' +
+             '"xml" parameter file.')
+@click.argument("xml_path", type=click.Path(exists=True))
+@click.option("--directory", "-d", type=click.Path(exists=True))
+@click.option("--generate", "-g", isflag=True)
+def generate_(name, xml_path, **kwargs):
+    io = MiindIO(xml_path, kwargs['directory'])
+    if kwargs['generate']:
+        io.generate()
+    io.run()
 
 
 def main():
